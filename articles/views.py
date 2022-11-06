d064385addd8409cab86a65a78f295a64b7a944d
Alena1945 <alena.belousova.2811@mail.ru>
2022-11-06 15:06:11 +0500

    Initial commit

--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from .models import Article, Scope, ArticleScope
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
+    ordering = '-published_at'
+    context = {'articles': Article.objects.order_by(ordering).all()}
+    # for article in context['articles']:
+    #     all_article_scopes = ArticleScope.objects.filter(article__id=article[id])
+        #same_all_items_with_breakfast_menu = breakfast.item_set.all()
+    #    print (all_article_scopes)
+    print(context)
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
 
     return render(request, template, context)
